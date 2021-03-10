60f90c6b104b024236751ef2b9c79b589d8f7967
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-03-10 07:50:05 +0100

    Task_0

--- src/tower_sim.cpp
+++ src/tower_sim.cpp
@@ -7,6 +7,7 @@
 #include "img/image.hpp"
 #include "img/media_path.hpp"
 
+#include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <ctime>
@@ -40,7 +41,6 @@ void TowerSimulation::create_aircraft(const AircraftType& type) const
     const Point3D direction = (-start).normalize();
 
     Aircraft* aircraft = new Aircraft { type, flight_number, start, direction, airport->get_tower() };
-    GL::display_queue.emplace_back(aircraft);
     GL::move_queue.emplace(aircraft);
 }
 
@@ -57,6 +57,9 @@ void TowerSimulation::create_keystrokes() const
     GL::keystrokes.emplace('+', []() { GL::change_zoom(0.95f); });
     GL::keystrokes.emplace('-', []() { GL::change_zoom(1.05f); });
     GL::keystrokes.emplace('f', []() { GL::toggle_fullscreen(); });
+    GL::keystrokes.emplace('z', []() { GL::ticks_per_sec = std::max(GL::ticks_per_sec - 1u, 1u); });
+    GL::keystrokes.emplace('a', []() { GL::ticks_per_sec = std::min(GL::ticks_per_sec + 1u, 180u); });
+    GL::keystrokes.emplace('p', []() { GL::is_paused = !GL::is_paused; });
 }
 
 void TowerSimulation::display_help() const
@@ -77,7 +80,6 @@ void TowerSimulation::init_airport()
     airport = new Airport { one_lane_airport, Point3D { 0, 0, 0 },
                             new img::Image { one_lane_airport_sprite_path.get_full_path() } };
 
-    GL::display_queue.emplace_back(airport);
     GL::move_queue.emplace(airport);
 }
 
