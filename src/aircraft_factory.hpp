e0b16fbc5c970c912dcd2896b1e1c30181c4305d
Killian Blanchard <kblanc01@etud.u-pem.fr>
2021-03-18 09:30:43 +0100

    Task1 - Objective2 - A

---
+++ src/aircraft_factory.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "aircraft_types.hpp"
+#include "airport.hpp"
+
+class Aircraft;
+
+class AircraftFactory
+{
+private:
+    const static size_t NUM_AIRCRAFT_TYPES = 3;
+    const std::string airlines[8] = { "AF", "LH", "EY", "DL", "KL", "BA", "AY", "EY" };
+    AircraftType* aircraft_types[NUM_AIRCRAFT_TYPES] {};
+
+
+
+public:
+    void init_aircraft_types();
+
+    [[nodiscard]] std::unique_ptr<Aircraft> create_aircraft(const AircraftType& type, Airport* airport) const;
+
+    [[nodiscard]] std::unique_ptr<Aircraft> create_random_aircraft(Airport* airport) const;
+};
